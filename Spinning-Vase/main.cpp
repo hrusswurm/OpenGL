a1288f4b3362ddab1d1b4b131e3651890bd48fc9
hrusswurm <hrusswur@uwo.ca>
2016-12-21 14:02:19 -0500

    Removed student# and email

--- Spinning-Vase/main.cpp
+++ Spinning-Vase/main.cpp
@@ -1,8 +1,6 @@
 /*
 Hannah Russwurm
 Oct. 28, 2016
-hrusswur@uwo.ca
-250662242
 The purpose of this program is to implement a wiremesh renderer and output a vase created by reading vertex points from a text file. 
 */
 
